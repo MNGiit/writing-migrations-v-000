5c702511b8825fc9a4311a55da0f40a08e111169
Mohammed Noori <kmanoorib@gmail.com>
2018-11-09 01:19:52 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,8 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
 
-#this is how to use add_column  
+#this is how to use add_column
+#but this time we're changing add_column to change_column
 #class AddFavoriteFoodToArtists < ActiveRecord::Migration
 #  def change
 #    add_column :artists, :favorite_food, :string
