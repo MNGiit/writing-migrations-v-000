d652bf4e7792587d84e3e7093d8f7b8f81df1bd9
Mohammed Noori <kmanoorib@gmail.com>
2018-11-09 01:18:42 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,12 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
-  
-  
+
+
+#this is how to use add_column  
+#class AddFavoriteFoodToArtists < ActiveRecord::Migration
+#  def change
+#    add_column :artists, :favorite_food, :string
+#  end
+#end
   
   def change
     add_column :students, :grade, :integer
