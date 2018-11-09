4d734f23ad0d43621b56b46bf89ee329e337f03d
Mohammed Noori <kmanoorib@gmail.com>
2018-11-09 01:15:37 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,8 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :grade
-      t.integer :birthdate
     end
   end
 
